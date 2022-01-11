65abc5f82cc02178e5ee525b6f5009c0e1465308
Tajemniczy <SFBreakTF2@gmail.com>
2022-01-12 00:17:41 +0100

    Add project files.

---
+++ PluginHandler.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PluginHandler.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+namespace Mistaken.CustomCuffing
+{
+    /// <inheritdoc/>
+    public class PluginHandler : Plugin<Config, Translation>
+    {
+        /// <inheritdoc/>
+        public override string Author => "Mistaken Devs";
+
+        /// <inheritdoc/>
+        public override string Name => "CustomCuffing";
+
+        /// <inheritdoc/>
+        public override string Prefix => "MCustomCuffing";
+
+        /// <inheritdoc/>
+        public override PluginPriority Priority => PluginPriority.Medium;
+
+        /// <inheritdoc/>
+        public override Version RequiredExiledVersion => new Version(4, 2, 2);
+
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+            Instance = this;
+
+            new CustomCuffingHandler(this);
+
+            API.Diagnostics.Module.OnEnable(this);
+
+            base.OnEnabled();
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+            API.Diagnostics.Module.OnDisable(this);
+
+            base.OnDisabled();
+        }
+
+        internal static PluginHandler Instance { get; private set; }
+    }
+}
